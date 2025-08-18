47cd33fdf45589fad89483e0ad8b6fcb15b791a6
UrbanAfrique <urbanafrique2023@gmail.com>
2025-08-18 02:46:13 +0200

    Fix RequireAuth import error

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RequireAuth from "./components/RequireAuth";
+import RequireAuth from "./components/RequireAuth";
 import Index from "./pages/Index";
 import Products from "./pages/Products";
 import Hotels from "./pages/products/Hotels";
